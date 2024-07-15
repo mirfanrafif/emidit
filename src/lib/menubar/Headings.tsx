603f3a54a1fa2e0491e7736edf3775e9d78442a9
Rafif <mirfanrafif17@gmail.com>
2024-07-15 16:29:31 +0700

    fix: styling

---
+++ src/lib/menubar/Headings.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import styles from './Menubar.module.css';
+
+// 24, 20, 18, 16, 14, 12
+
+const headingLevels: {
+  level: 1 | 2 | 3 | 4 | 5 | 6;
+  size: number;
+}[] = [
+  { level: 1, size: 24 },
+  { level: 2, size: 20 },
+  { level: 3, size: 18 },
+  { level: 4, size: 16 },
+  { level: 5, size: 14 },
+  { level: 6, size: 12 },
+];
+
+const Headings = (props: {
+  onSelectHeading: (level: 1 | 2 | 3 | 4 | 5 | 6) => void;
+}) => {
+  const [showPopup, setShowPopup] = React.useState(false);
+
+  return (
+    <span className={styles.addTableAnchor}>
+      <button
+        onClick={() => setShowPopup(!showPopup)}
+        className={styles.addTableButton}
+      >
+        Headings
+      </button>
+
+      {showPopup && (
+        <div className={styles.popup}>
+          {headingLevels.map((heading) => (
+            <button
+              key={heading.level}
+              onClick={() => {
+                props.onSelectHeading(heading.level);
+                setShowPopup(false);
+              }}
+            >
+              Heading {heading.level} ({heading.size}px)
+            </button>
+          ))}
+        </div>
+      )}
+    </span>
+  );
+};
+
+export default Headings;
--- src/lib/menubar/Menubar.tsx
+++ src/lib/menubar/Menubar.tsx
@@ -1,62 +1,22 @@
 import React from 'react';
 
-import styles from '../TextEditor.module.css';
+import styles from './Menubar.module.css';
 import { Editor } from '@tiptap/react';
 import AddTableMenu from './AddTableMenu';
 import AddButton from './AddButton';
 import AddImage from './AddImage';
-import SetColorPopup from './SetColor';
+import Headings from './Headings';
 
 const Menubar = (props: { editor: Editor | null }) => {
   const { editor } = props;
 
-  const timeoutRef = React.useRef<NodeJS.Timeout | null>(null);
-
   return (
     <div className={styles.toolbar}>
-      <button
-        onClick={() => {
-          editor?.chain().focus().toggleHeading({ level: 1 }).run();
+      <Headings
+        onSelectHeading={(level) => {
+          editor?.chain().focus().setHeading({ level }).run();
         }}
-      >
-        h1
-      </button>
-      <button
-        onClick={() => {
-          editor?.chain().focus().toggleHeading({ level: 2 }).run();
-        }}
-      >
-        h2
-      </button>
-      <button
-        onClick={() => {
-          editor?.chain().focus().toggleHeading({ level: 3 }).run();
-        }}
-      >
-        h3
-      </button>
-      <button
-        onClick={() =>
-          editor?.chain().focus().toggleHeading({ level: 4 }).run()
-        }
-      >
-        h4
-      </button>
-      <button
-        onClick={() =>
-          editor?.chain().focus().toggleHeading({ level: 5 }).run()
-        }
-      >
-        h5
-      </button>
-      <button
-        onClick={() =>
-          editor?.chain().focus().toggleHeading({ level: 6 }).run()
-        }
-      >
-        h6
-      </button>
-
+      />
       <button
         onClick={() => {
           editor?.chain().focus().toggleTextHighlight().run();
@@ -67,23 +27,20 @@ const Menubar = (props: { editor: Editor | null }) => {
       <button onClick={() => editor?.chain().focus().setParagraph().run()}>
         paragraph
       </button>
-
+      |
       <button onClick={() => editor?.chain().focus().setBold().run()}>
         bold
       </button>
-
       <button onClick={() => editor?.chain().focus().setItalic().run()}>
         italic
       </button>
-
       <button onClick={() => editor?.chain().focus().setStrike().run()}>
         strike
       </button>
-
       <button onClick={() => editor?.chain().focus().setUnderline().run()}>
         underline
       </button>
-
+      |
       <button
         onClick={() => {
           editor?.chain().focus().setTextAlign('left').run();
@@ -91,7 +48,6 @@ const Menubar = (props: { editor: Editor | null }) => {
       >
         align left
       </button>
-
       <button
         onClick={() => {
           editor?.chain().focus().setTextAlign('center').run();
@@ -99,7 +55,6 @@ const Menubar = (props: { editor: Editor | null }) => {
       >
         align center
       </button>
-
       <button
         onClick={() => {
           editor?.chain().focus().setTextAlign('right').run();
@@ -107,13 +62,12 @@ const Menubar = (props: { editor: Editor | null }) => {
       >
         align right
       </button>
-
+      |
       <AddTableMenu
         onAddTable={(rows, cols) => {
           editor?.chain().focus().insertTable({ rows, cols }).run();
         }}
       />
-
       <AddButton
         onAddButton={(text, href) => {
           editor?.commands.addButton({
@@ -122,7 +76,6 @@ const Menubar = (props: { editor: Editor | null }) => {
           });
         }}
       />
-
       <input
         type="color"
         onChange={(event) => {
@@ -131,7 +84,6 @@ const Menubar = (props: { editor: Editor | null }) => {
         value={props.editor?.getAttributes('textStyle').color ?? '#000000'}
         data-testid="setColor"
       />
-
       <AddImage
         onAddImage={(href) => {
           editor
